10be82513d498eb502a60291ed22239cadc00b1c
mxxt1 <matt@dustcloudlabs.com>
2019-11-11 18:03:48 -0500

    MVP - register, login, get users endpoints work. Register and login hash/check hash against pw; /user requires requireAuth, validation is working correctly

--- api/auth-router.js
+++ api/auth-router.js
@@ -5,5 +5,39 @@ const bcrypt = require('bcryptjs');
 const api = require('./api-model');
 
 
+// /user/auth/
+
+router.post('/login', (req, res) => {
+    let {username, password} = req.headers;
+
+    api.findBy({username})
+    .first()
+    .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)){
+            res.status(200).json({message: `Welcome ${user.username}`});
+        } else {
+            res.status(401).json({error: `${user.username} not found`});
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err); 
+    });
+});
+
+router.post('/register', (req, res) => {
+   let user = req.body;
+   
+   let hashedPass = bcrypt.hashSync(user.password, 12);
+
+   user.password = hashedPass;
+
+   api.addUser(user)
+   .then(response => {
+       res.status(201).json(response)
+   })
+   .catch(error => {
+       res.status(500).json(error);
+   }) 
+})
 
 module.exports = router;
